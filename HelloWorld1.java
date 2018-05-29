ba810e5320a529997b5a272a8769b1f95b71a392
dhivyamuthusamy <dhivyamuthusamy182@gmail.com>
2018-05-29 01:32:11 -0700

    world

---
+++ HelloWorld1.java
@@ -0,0 +1,9 @@
+public class HelloWorld1{
+  public static void main(String s[]){
+System.out.println("welcome");
+byte one = 100;
+byte two = 20;
+int three = one + two ;
+System.out.println(three);
+}
+}
